b3f414fe4a36559d38d939bb20708197bc9bc770
martin <jmartinaguirre79@gmail.com>
2022-07-23 14:15:21 -0300

    Cración archivo clase 5

--- js/clase4.js
+++ js/clase4.js
@@ -1,7 +1,25 @@
 //FUNCIONES SIMPLES
-
+/*
 function saludar(){
     alert("Bienvenido")
 };
 //SIEMPRE PONER PARENTESIS
 saludar ();
+*/
+
+
+//FUNCIONES con parámetros
+/*
+function msj (nombre , apellido){
+    alert(nombre + " " + apellido)
+}
+
+msj("martin" , "Aguirre")
+
+*/
+
+
+//funciones flecha
+const suma = (a, b) => a +b
+
+return (suma(10,10))
